89eab94a0505b55a0b7de110bcec31d303282867
Itunu <adeyemoitunu1@gmail.com>
2022-06-07 10:19:54 +0100

    project

---
+++ code/script.js
@@ -0,0 +1,16 @@
+function Menu(e) {
+    let list = document.querySelector('.newul');
+    let menu2 = document.querySelector('.menu2');
+    e.name === 'menu' ? (e.name = "close", 
+    list.classList.add('top-[80px]'), 
+    menu2.classList.add('mt-64'),
+    list.classList.add('opacity-100')) : (e.name = "menu", 
+    list.classList.remove('top-[80px]'), 
+    menu2.classList.remove('mt-64'),
+    list.classList.remove('opacity-100'))
+
+    // e.name === 'menu2' ? (e.name = "close",
+    // menu2.classList.add('pb-40')): (e.name = 'menu2',
+    // menu2.classList.remove('pb-40'))
+}
+
